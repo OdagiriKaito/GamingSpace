81fd8ed5f2c595d1eff1cc95c36152b811702a9e
まっち屋1 <matchiya1@matchiya1-no-MacBook-Pro.local>
2017-07-14 17:35:27 +0900

    サブカテゴリ表示

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
+<!doctype html>
 <html>
 <head>
+<meta charset="utf-8" />
   <script src="https://code.jquery.com/jquery-3.2.1.js"
   integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE="
   crossorigin="anonymous">
@@ -8,12 +10,21 @@
 ul{
   list-style: none;
 }
-
-
     .subcategory{
       display:none;
     }
-
+    body{
+        background-color:f00cc0;
+        overflow-x: hidden;
+    }
+    #category-container{
+      overflow-x: scroll;
+      margin-bottom: 0px;
+    }
+    #category-list {
+      border: 1px solid #000;
+      width: 5000px;
+    }
     #category-list .category{
       width: 200px;
       padding: 10px;
@@ -31,7 +42,13 @@ ul{
       border:1px solid #000;
       border-radius: 5px;
       cursor: pointer;
+      background-color: #00ff7f
     }
+
+    #category-list .subcategory{
+          width: 150px;
+          position: absolute;
+        }
 
     #category-list .subcategory a{
       text-decoration: none;
@@ -65,7 +82,8 @@ ul{
     };
 
     function clickCategory(){
-      $(this).closest('li').find('.subcategory').slideToggle();
+      $('.subcategory').hide();
+      $(this).next('.subcategory').slideToggle();
     }
 
     $(':input[name="category"]').change();
@@ -83,55 +101,57 @@ $stmt =$db->query($sql);
 
 $result=$stmt->fetchAll();
 ?>
-<form action="/thread_result.php" method="post">
-<table border="0" cellspacing="1" width="" class="form-table">
-<tbody>
-  <tr>
-    <td colspan="2">
-     カテゴリを選択</br>
-     <ul id="category-list">
-  <?php
+カテゴリを選択</br>
+<section id="category-container">
+<section id="category-list">
+<?php
 $sql = 'select * from subcategory order by cat_id, id';
 $stmt = $db->query($sql);
 $res = $stmt->fetchAll();
 $subcategorys = array();
 foreach($res as $row){
-  if(!isset($subcategorys[$row['cat_id']])){
-    $subcategorys[$row['cat_id']] = array();
-  }
-  array_push($subcategorys[$row['cat_id']],
-        array(
-              'id' => $row['id'],
-              'name' => $row['subcategory_name']
-            )
-        );
+if(!isset($subcategorys[$row['cat_id']])){
+$subcategorys[$row['cat_id']] = array();
+}
+array_push($subcategorys[$row['cat_id']],
+   array(
+         'id' => $row['id'],
+         'name' => $row['subcategory_name']
+       )
+   );
 }
 
-    $sql='select * from category';
-    $stmt =$db->query($sql);
-    $categorys=$stmt->fetchAll();
-    foreach($categorys as $row){
-  ?>
-  <li>
-    <span class="category"><?php echo $row['category_name']?></span>
-  <ul class='subcategory'>
-    <?php
-      foreach($subcategorys[$row['id']] as $key =>$subcategory){
-    ?>
-      <li>
-        <a href="/thread_list.php?id=<?php echo $row['id'] ?>&subcat_id=<?php echo $subcategory['id'] ?>">
-          <?php echo $subcategory['name'] ?>
-        </a>
-      </li>
-      <?php
-      }
-      ?>
-     </ul>
-  </li>
-  <?php
-  }
-  ?>
-    </ul>
+$sql='select * from category';
+$stmt =$db->query($sql);
+$categorys=$stmt->fetchAll();
+foreach($categorys as $row){
+?>
+<span class="category"><?php echo $row['category_name']?></span>
+<ul class='subcategory'>
+<?php
+ foreach($subcategorys[$row['id']] as $key =>$subcategory){
+?>
+ <li>
+   <a href="/thread_list.php?id=<?php echo $row['id'] ?>&subcat_id=<?php echo $subcategory['id'] ?>">
+     <?php echo $subcategory['name'] ?>
+   </a>
+ </li>
+ <?php
+ }
+ ?>
+</ul>
+<?php
+}
+?>
+</section>
+</section>
+
+<form action="/thread_result.php" method="post">
+<table border="0" cellspacing="1" width="" class="form-table">
+<tbody>
+  <tr>
+    <td colspan="2">
+
 <ul>
 <?php
   if($_GET["id"]){
